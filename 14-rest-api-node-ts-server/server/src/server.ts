4416145bfbdc8fb9ef6da6e4875ac10b130b57c1
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-04-17 16:24:31 -0500

    424

--- 14-rest-api-node-ts-server/server/src/server.ts
+++ 14-rest-api-node-ts-server/server/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import router from './router'
 import db from './config/db'
 import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec, { swaggerUIOptions } from './config/swagger';
 
@@ -17,14 +19,29 @@ export async function connectDB() {
 connectDB()
 const server = express()
 
+const corsOptions: CorsOptions = {
+
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
+    }
+  }
+}
+
+server.use(cors(corsOptions))
+
 server.use(express.json())
+
+server.use(morgan('dev'))
+
 server.use('/api/products', router)
 
 server.get('/api', (req, res) => {
   res.json({ msg: 'Desde API' })
 })
 
-// Aqui se coloca la configuración, luego de swaggerSpec
 server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUIOptions))
 
 
