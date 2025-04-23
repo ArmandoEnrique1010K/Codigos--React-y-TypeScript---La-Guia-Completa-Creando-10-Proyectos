35fd969d6f389435ead7876c75e44471ae002707
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-04-23 00:01:25 -0500

    463

---
+++ 15-uptask-mern-full-stack/uptask_backend/src/routes/projectRoutes.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import { body, param } from "express-validator"
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middlewares/validation";
+
+const router = Router();
+
+router.post('/',
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+  body('description')
+    .notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+  handleInputErrors,
+  ProjectController.createProject
+)
+
+router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  ProjectController.getProjectById)
+
+router.put('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+  body('description')
+    .notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+  handleInputErrors,
+  ProjectController.updateProject)
+
+// Endpoint para eliminar un proyecto
+router.delete('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  ProjectController.deleteProject)
+
+
+export default router
