85dd9f5c3be54a06c63ae9c303aa953ed88f7cd0
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-05-04 18:45:28 -0500

    539 - seccion 38

--- 15-uptask-mern-full-stack/uptask_backend/src/server.ts
+++ 15-uptask-mern-full-stack/uptask_backend/src/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
+import authRoutes from './routes/authRoutes'
 import { corsConfig } from './config/cors'
 import morgan from 'morgan'
 
@@ -18,6 +19,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 
+app.use('/api/auth', authRoutes)
 app.use('/api/projects', projectRoutes)
 
 export default app;
