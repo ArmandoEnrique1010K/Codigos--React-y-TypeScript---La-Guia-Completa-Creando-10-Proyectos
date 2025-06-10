b806d9e501cee13726efa375f5143f590ad36b0d
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-09 20:33:02 -0500

    590. Modificando el Modelo para tener un equipo

--- 15-uptask-mern-full-stack/uptask_backend/src/models/Project.ts
+++ 15-uptask-mern-full-stack/uptask_backend/src/models/Project.ts
@@ -10,6 +10,9 @@ export interface IProject extends Document {
 
   // Añade una nueva propiedad, la persona que creo el proyecto (un usuario)
   manager: PopulatedDoc<IUser & Document>
+
+  // Añade la propiedad para el equipo asignado a un proyecto
+  team: PopulatedDoc<IUser & Document>[]
 }
 
 const ProjectSchema: Schema = new Schema({
@@ -38,8 +41,17 @@ const ProjectSchema: Schema = new Schema({
   manager: {
     type: Types.ObjectId,
     ref: 'User'
-  }
+  },
+  // Equipo de usuarios
+  team: [
+    {
+      type: Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  // Agrega un nuevo proyecto desde el frontend para ver que se tenga esa propiedad team (inicialmente un arreglo vacio)
 }, { timestamps: true })
+
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema)
 export default Project
