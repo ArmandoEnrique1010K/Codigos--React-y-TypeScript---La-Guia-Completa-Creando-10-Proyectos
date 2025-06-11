585da5dbc61d7891f57c4bb2f3edcbf3cb8cc191
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-10 21:03:10 -0500

    599. Agregando el usuario al proyecto

--- 15-uptask-mern-full-stack/uptask_frontend/src/api/TeamAPI.ts
+++ 15-uptask-mern-full-stack/uptask_frontend/src/api/TeamAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, TeamMemberForm } from "../types";
+import { Project, TeamMember, TeamMemberForm } from "../types";
 
 // Llamada hacia la API para buscar usuario por email (requiere el id del proyecto y los datos del formulario)
 export async function findUserByEmail({ projectId, formData }: { projectId: Project['_id'], formData: TeamMemberForm }) {
@@ -16,6 +16,20 @@ export async function findUserByEmail({ projectId, formData }: { projectId: Proj
     return response.data
   } catch (error) {
     // Si hay algun mensaje de error
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+// Agrega un usuario al proyecto
+export async function addUserToProject({ projectId, id }: { projectId: Project['_id'], id: TeamMember['_id'] }) {
+  try {
+    const url = `/projects/${projectId}/team`
+    // El id se tiene que enviar dentro de un objeto
+    const { data } = await api.post(url, { id })
+    return data
+  } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
     }
--- 15-uptask-mern-full-stack/uptask_frontend/src/components/team/SearchResult.tsx
+++ 15-uptask-mern-full-stack/uptask_frontend/src/components/team/SearchResult.tsx
@@ -1,4 +1,8 @@
+import { useMutation } from "@tanstack/react-query";
 import { TeamMember } from "@/types/index";
+import { addUserToProject } from "@/api/TeamAPI";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 type SearchResultProps = {
   user: TeamMember;
@@ -6,14 +10,46 @@ type SearchResultProps = {
 
 export default function SearchResult({ user }: SearchResultProps) {
   // Antes de asignar el type, imprime el usuario que ha sido encontrado
-  console.log(user);
+  // console.log(user);
+
+  // Extrae el id del proyecto de la URL como parametro
+  const params = useParams();
+  const projectId = params.projectId!; // Siempre habra un projectId
+
+  // Instancia de useMutation para el manejo de la función de agregar miembro al proyecto
+  const { mutate } = useMutation({
+    mutationFn: addUserToProject,
+    onError: (error) => {
+      // Utiliza las notificaciones Toast
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data); // Recuerda que la respuesta de la petición es un String con un mensaje
+    },
+  });
+
+  const handleAddUserToProject = () => {
+    const data = {
+      projectId,
+      id: user._id, // Como el id esta dentro de un objeto, se llama a la propiedad _id de user
+    };
+
+    // Muta los datos
+    mutate(data);
+  };
+
+  // Cuando agregues el usuario desde el frontend (luego de pulsar el boton de agregar usuario cuando ha sido encontrado, revisa la base de datos con MongoDB Compass)
+  // Luego pulsa el mismo boton otra vez, y podras ver el mensaje de error de que el usuario ya existe en el proyecto
 
   return (
     <>
       <p className="mt-10 text-center font-bold">Resultado:</p>
       <div className="flex justify-between items-center">
         <p>{user.name}</p>
-        <button className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer">
+        <button
+          className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer"
+          onClick={handleAddUserToProject}
+        >
           Agregar al Proyecto
         </button>
       </div>
