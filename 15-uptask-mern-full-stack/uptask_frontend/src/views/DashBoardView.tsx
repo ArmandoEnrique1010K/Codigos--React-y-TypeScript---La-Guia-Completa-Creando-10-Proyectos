18aaba7a00212a560eb536136673ab0506755647
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-10 22:34:11 -0500

    606. Agregando un indicador del rol del usuario

--- 15-uptask-mern-full-stack/uptask_frontend/src/views/DashBoardView.tsx
+++ 15-uptask-mern-full-stack/uptask_frontend/src/views/DashBoardView.tsx
@@ -74,6 +74,22 @@ export default function DashBoardView() {
               >
                 <div className="flex min-w-0 gap-x-4">
                   <div className="min-w-0 flex-auto space-y-2">
+                    <div className="mb-2">
+                      {
+                        // Comprueba de que si el manager del proyecto es el usuario autenticado para mostrar el rol de manager o miembro del equipo
+                        project.manager === user?._id ? (
+                          <p className="font-bold text-xs uppercase bg-indigo-50 text-indigo-500 border-2 border-indigo-500 rounded-lg inline-block py-1 px-5">
+                            Manager
+                          </p>
+                        ) : (
+                          <p className="font-bold text-xs uppercase bg-indigo-50 text-green-500 border-2 border-green-500 rounded-lg inline-block py-1 px-5">
+                            Miembro del equipo
+                          </p>
+                        )
+
+                        // El manejo de roles es muy frecuente en la programación fullstack
+                      }
+                    </div>
                     <Link
                       to={`/projects/${project._id}`}
                       className="text-gray-600 cursor-pointer hover:underline text-3xl font-bold"
