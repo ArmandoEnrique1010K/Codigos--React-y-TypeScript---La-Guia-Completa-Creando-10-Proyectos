36455c48cc65bc72e7ef7392b1e9942d481a5a00
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-13 20:26:32 -0500

    639. Primeros pasos con DNDKIT

--- 15-uptask-mern-full-stack/uptask_frontend/src/main.tsx
+++ 15-uptask-mern-full-stack/uptask_frontend/src/main.tsx
@@ -15,3 +15,17 @@ createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </StrictMode>
 );
+
+// Libreria de Drag and Drop
+// Puedes arrastrar una tarea hacia un nuevo estado
+// Para aquello puedes utilizar la libreria mencionada, contiene 2 hooks
+// useDroppable --> es donde se va a arrastrar el elemento
+// useDraggable --> el elemento que se va a rrastrar
+
+// Contexto de Drag and Drop
+// DndContext --> se registra un contexto para contener las acciones
+
+// Instalación: ejecuta el comando: npm install @dnd-kit/core
+
+// Documentación: https://dndkit.com/ & https://docs.dndkit.com/
+// Ejemplos: https://examples.dndkit.com/, el hook useDragable tiene varias opciones
