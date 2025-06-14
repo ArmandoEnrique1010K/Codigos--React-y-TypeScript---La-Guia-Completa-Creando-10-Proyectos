fa895f283c184d6d6c4bda531f08a0a307341faf
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-13 21:21:56 -0500

    644. Añadiendo un Efecto de Drag

--- 15-uptask-mern-full-stack/uptask_frontend/src/components/tasks/DropTask.tsx
+++ 15-uptask-mern-full-stack/uptask_frontend/src/components/tasks/DropTask.tsx
@@ -13,13 +13,25 @@ export default function DropTask({ status }: DropTaskProps) {
   // Dropable retorna
   // setNodeRef: El elemento del codigo HTML, en el que se aplicara la funcionalidad
 
+  // Imprime varias veces false porque hay tareas, imprime un true si la tarea fue arrastrada hacia el componente que contiene la función setNodeRef
+  // console.log(isOver);
+
+  // Sintaxis de JavaScript para aplicar estilos
+  const style = {
+    opacity: isOver ? 0.4 : undefined,
+    // backgroundColor: "red"
+  };
+
   return (
     <div
       // Llama a la función setNodeRef para aplicar la funcionalidad
       ref={setNodeRef}
+      // Aplica el estilo
+      style={style}
       className="text-xs font-semibold uppercase p-2 border border-dashed border-slate-500 mt-5 grid place-content-center text-slate-500"
     >
-      Soltar tarea aqui - {status}
+      {/* No muestres la propiedad status */}
+      Soltar tarea aqui
     </div>
   );
 }
