c5f069b976a1ed459fd30c5b3410fa1d2a148828
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-22 14:03:44 -0500

    676. Agregar articulos a la orden

--- 16-quiosco-next/src/store.ts
+++ 16-quiosco-next/src/store.ts
@@ -9,12 +9,30 @@ interface Store {
   addToOrder: (product: Product) => void
 }
 
-export const useStore = create<Store>(() => ({
+// Llama a la función set para escribir en el state
+export const useStore = create<Store>((set) => ({
   // Valores iniciales de los estados
   order: [],
 
   // Función para agregar a la orden un producto
   addToOrder: (product) => {
-    console.log('Agregando', product)
+
+    // Al imprimir el product, se tiene un objeto con propiedades innecesarias del producto como categoryId e image
+    // console.log('Agregando', product)
+
+    // Desestructura las propieades de product, utiliza el operador rest para tomar los demás propiedades necesarias
+    const { categoryId, image, ...data } = product
+
+    // Imprime las propiedades necesarias
+    // console.log(data);
+
+    set((state) => ({
+      // Se necesita escribir en el state de order las propieades quantity y subtotal
+      order: [...state.order, {
+        ...data,
+        quantity: 1,
+        subtotal: 1 * product.price
+      }]
+    }))
   }
 }))
