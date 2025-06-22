7201baa482fada0b8a2d5d05f7e9097ec0d772b2
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-21 23:41:30 -0500

    672. Resaltando la categoría actual

--- 16-quiosco-next/components/ui/CategoryIcon.tsx
+++ 16-quiosco-next/components/ui/CategoryIcon.tsx
@@ -1,17 +1,43 @@
+// Convierte este componente a un componente de cliente
+"use client";
+
 import { Category } from "@prisma/client";
 import Image from "next/image";
 // Nextjs ofrece un componente de navegación para navegar con un gran performance a traves de diferentes paginas, se llama Link
 import Link from "next/link";
+
+// Nextjs ofrece un hook useParams (similar al de React)
+import { useParams } from "next/navigation";
 
 // Cuando defines los schemas de prisma, ya sea category o product, ellos tambien generan los types, toma el type de @prisma/client
 type CategoryIconProps = {
   category: Category;
 };
 
-export default function CategoryIcon({ category }: CategoryIconProps) {
+export default function CategoryIcon({
+  category /*, params */,
+}: CategoryIconProps) {
+  // Si imprimes los parametros de la URL muestra undefined, params no esta disponible en algunos componentes,
+  // console.log(params);
+
+  // Considera que params solamente esta disponibles en los archivos de tipo layout.tsx, page.tsx, route.tsx y generateMetadata
+
+  // Instancia del hook useParams (solamente funciona en componentes de tipo client, se requiere añadir 'use client' para convertirlo a un componente de cliente)
+  // Para tener el autocompletado de typescript, añade el generic con la propiedad del parametro de la URL
+  const params = useParams<{ category: string }>();
+  // console.log(params);
+  // https://nextjs.org/docs/app/api-reference/functions/use-params
+
+  // En la consola del navegador aparece multiples veces el nombre de la categoria, recuerda que este codigo se ejecuta por cada componente que se renderiza (cada categoria)
+
+  // En la consola del servidor aparecne varios logs de consultas a la base de datos, ve hacia otra categoria y luego retorna a la misma categoria anterior y notaras que no vuelve a realizar la consulta anterior (reutiliza las consultas, cache de nextjs)
+
   return (
     <div
-      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+      // Verifica si la categoria seleccionada es igual a la categoria actual para aplicarle un color de fondo
+      className={`${
+        category.slug === params.category ? "bg-amber-400" : ""
+      } flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
       {/* Establece un ancho y alto en el div padre para que controle el tamaño de la imagen */}
       <div className="w-16 h-16 relative">
