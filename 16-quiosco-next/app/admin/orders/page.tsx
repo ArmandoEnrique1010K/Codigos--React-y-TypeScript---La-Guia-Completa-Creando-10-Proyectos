54cb2bcc459f6c6cd368ab65251764bd7fde0918
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-22 19:06:17 -0500

    697. Mostrar las Ordenes Pendientes

--- 16-quiosco-next/app/admin/orders/page.tsx
+++ 16-quiosco-next/app/admin/orders/page.tsx
@@ -1,10 +1,56 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function OrdersPage() {
+async function getPendingOrders() {
+  // Trae todas las ordenes, cuyo estado es false
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+
+    // include sirve para traer los contenidos de la orden del usuario
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return orders;
+}
+
+// Esta pagina se vuelve asincrona
+export default async function OrdersPage() {
+  // Imprime en la consola del servidor las ordenes
+  const orders = await getPendingOrders();
+  // console.log(orders);
+
+  // Como el campo orderProducts trae los objetos como [Object], debes convertir orders a String
+  // Los parametros:  null indica que no se van a modificar los elementos y 2 indica el espaciado
+  console.log(JSON.stringify(orders, null, 2));
+
+  // El resultado que se ve en la consola es un objeto JSON que contiene todos los datos de la orden, incluyendo los datos de los productos de la orden
+
   return (
     <>
       {/* Llama al componente para mostrar el titulo */}
       <Heading>Admininistrar ordenes</Heading>
+
+      {orders.length ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+          {
+            // Itera con orders para mostrar un componente por cada orden
+            orders.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))
+          }
+        </div>
+      ) : (
+        <p className="text-center">No hay ordenes pendientes</p>
+      )}
     </>
   );
 }
--- 16-quiosco-next/src/types/index.ts
+++ 16-quiosco-next/src/types/index.ts
@@ -1,8 +1,15 @@
 // Crea los types personalizados (lo que Prisma no puede crear por defecto)
-import { Product } from "@/app/generated/prisma";
+import { Order, OrderProducts, Product } from "@prisma/client";
 
 // Type para la orden
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
   quantity: number
   subtotal: number
+}
+
+// Type para una orden con productos (se asignan los types de prisma), es el type de Order y se asigna la propiedad orderProducts, que es de tipo arreglo
+export type OrderWithProducts = Order & {
+  orderProducts: (OrderProducts & {
+    product: Product
+  })[]
 }
