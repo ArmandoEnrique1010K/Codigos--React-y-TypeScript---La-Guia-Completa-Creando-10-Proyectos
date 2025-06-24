d4dab39ca782a4c21ed3ccd82ce3cc1c7a334b86
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-23 22:37:34 -0500

    709. Creando un Formulario de búsqueda de productos

--- 16-quiosco-next/app/admin/products/page.tsx
+++ 16-quiosco-next/app/admin/products/page.tsx
@@ -1,7 +1,9 @@
+import ProductSearchForm from "@/components/products/ProductSearchForm";
 import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 // Para limitar la cantidad de paginas, debes limitar segun la cantidad de productos
@@ -96,6 +98,18 @@ export default async function ProductsPage({
   return (
     <>
       <Heading>Administrar Productos</Heading>
+
+      {/* Boton para crear producto */}
+      <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+        <Link
+          href={"/admin/products/new"}
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        >
+          Crear Producto
+        </Link>
+
+        <ProductSearchForm />
+      </div>
 
       {/* Pasa los productos como prop al componente */}
       <ProductTable products={products} />
