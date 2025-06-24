22fcae57d5c4f8fe31b836321b51a9f5eb3c51bc
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-23 21:12:21 -0500

    704. Creando un Paginador
    El codigo fue modificado porque hubo una actualizacion con NextJS 14

--- 16-quiosco-next/app/admin/products/page.tsx
+++ 16-quiosco-next/app/admin/products/page.tsx
@@ -2,8 +2,22 @@ import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+async function getProducts(page: number, pageSize: number) {
+  // Si estas en la pagina 2, se salta los primeros 10 y muestra los siguietes productos
+  const skip = (page - 1) * pageSize;
+
   const products = await prisma.product.findMany({
+    // Crea un paginador, prisma ofrece las funciones: skip y take
+    // Cantidad de registros que se traera (similar a un limite en SQL)
+    // take: 10,
+    take: pageSize,
+
+    // Saltea los primeros n registros
+    // skip: 10,
+    skip, // skip: skip
+
+    // Controla las 2 funciones: take y skip para tener un paginador
+
     // Incluye los datos de la categoria
     include: {
       category: true,
@@ -16,8 +30,27 @@ async function getProducts() {
 // La tercera forma es definir un type basado en el valor que retorna una función, en este caso getProducts, retorna un arreglo de productos
 export type ProductWithCategory = Awaited<ReturnType<typeof getProducts>>;
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+// Se necesita que la URL tenga un string param
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  // Accede a: http://localhost:3000/admin/products?page=1
+  // Imprime el valor del query string page
+  // console.log(searchParams.page);
+
+  // Espera el objeto searchParams para extraer los parámetros
+  const { page = "1" } = await searchParams;
+
+  // Convierte 'page' a número y asigna un valor predeterminado si es inválido
+  const currentPage = parseInt(page as string, 10) || 1;
+  const pageSize = 10;
+
+  console.log(currentPage);
+
+  // Obtén los productos desde la base de datos
+  const products = await getProducts(currentPage, pageSize);
 
   // Imprime los productos en la consola
   // console.log(products);
