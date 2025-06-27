e41a87dda721f86216690f7e0f2f9ba623ae6ff6
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-06-27 00:02:59 -0500

    727. Revalidación Manual

--- 16-quiosco-next/app/admin/orders/page.tsx
+++ 16-quiosco-next/app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 async function getPendingOrders() {
   // Trae todas las ordenes, cuyo estado es false
@@ -30,14 +31,30 @@ export default async function OrdersPage() {
 
   // Como el campo orderProducts trae los objetos como [Object], debes convertir orders a String
   // Los parametros:  null indica que no se van a modificar los elementos y 2 indica el espaciado
-  console.log(JSON.stringify(orders, null, 2));
+  // console.log(JSON.stringify(orders, null, 2));
 
   // El resultado que se ve en la consola es un objeto JSON que contiene todos los datos de la orden, incluyendo los datos de los productos de la orden
+
+  const refreshOrders = async () => {
+    "use server";
+
+    // Revalida la URL de forma manual, cuando agregas una nueva orden, de forma automatica hace un fetch
+    revalidatePath("/admin/orders");
+  };
 
   return (
     <>
       {/* Llama al componente para mostrar el titulo */}
       <Heading>Admininistrar ordenes</Heading>
+
+      <form action={refreshOrders}></form>
+
+      {/* Pulsa el botón para actualizar las ordenes (mientras tengas abiertos 2 pestañas: una para el quiosco y el otro para las ordenes) */}
+      <input
+        type="submit"
+        value="Actualizar Ordenes"
+        className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+      />
 
       {orders.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
